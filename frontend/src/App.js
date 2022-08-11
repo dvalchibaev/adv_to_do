8ba0cfba907df75ff0df72864b486581651de6f7
dvalchibaev <alchibaevdv@gmail.com>
2022-08-11 11:05:27 +0300

    Lesson 2. CORS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,7 @@ class App extends React.Component{
   }
 
   componentDidMount(){
-    axios.get('http://127.0.0.1:8000/api/users')
-    .then(response => {
+    axios.get('http://127.0.0.1:8000/api/users').then(response => {
     const users = response.data
     this.setState(
     {
